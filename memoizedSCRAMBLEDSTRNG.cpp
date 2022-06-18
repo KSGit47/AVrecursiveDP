75f894939052e44f4da79a3f69fc9bdbfe2fd15e
KSGit47 <kushagra4adrenaline@gmail.com>
2022-06-19 02:55:51 +0530

    Memoized Scrambled String problem.

---
+++ memoizedSCRAMBLEDSTRNG.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstring>
+#include<unordered_map>
+using namespace std;
+unordered_map<string,int> mp;
+bool solve(string a, string b)
+{
+    if(a.compare(b)==0)
+    return true;
+    if(a.length()<=1)
+    return false;
+    string key=a;
+    key.push_back(' ');
+    key.append(b);
+    if(mp.find(key)!=mp.end())
+    return mp[key];
+    int n = a.length();
+    bool flag = false;
+    for (int i = 1; i < n; i++)
+    {
+        /* code */
+        if((solve(a.substr(0,i),b.substr(n-i,i)) && solve(a.substr(i,n-i),b.substr(0,n-i)))==true || (solve(a.substr(0,i),b.substr(0,i)) && solve(a.substr(i,n-i),b.substr(i,n-i)))==true )
+        {
+            flag=true;
+            break;
+        }
+    }
+    return mp[key]=flag;
+}
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    cout<<solve(a,b);
+}
